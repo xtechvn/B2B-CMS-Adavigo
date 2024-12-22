de734b0474ca301a21b8949aa3930cb8983ab90b
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2024-12-22 11:25:58 +0700

    fix voucher

--- ADAVIGO_FRONTEND/Models/Hotel/VoucherFEModel.cs
+++ ADAVIGO_FRONTEND/Models/Hotel/VoucherFEModel.cs
@@ -4,8 +4,24 @@ using System.Text;
 
 namespace ENTITIES.ViewModels.Voucher
 {
-    public class VoucherFEModel : ENTITIES.Models.Voucher
+    public class VoucherFEModel
     {
-        public int TotalRow { get; set; }
+        public int Id { get; set; }
+        public string code { get; set; }
+        public DateTime? cdate { get; set; }
+        public DateTime? udate { get; set; }
+        public DateTime? eDate { get; set; }
+        public int limitUse { get; set; }
+        public decimal? price_sales { get; set; }
+        public string unit { get; set; }
+        public int? rule_type { get; set; }
+        public bool? IsPublic { get; set; }
+        public string description { get; set; }
+        public bool? is_limit_voucher { get; set; }
+        public double? limit_total_discount { get; set; }
+        public double? MinTotalAmount { get; set; }
+        public int? campaign_id { get; set; }
+        public short? project_type { get; set; }
+        public int totalRow { get; set; }
     }
 }
